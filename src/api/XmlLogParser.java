bd6e0ccd654da787b17e850d45f0de33125bab0f
aorts <a.orts@highco-data.fr>
2018-02-07 11:03:39 +0100

    add ignore bin folder

--- src/api/XmlLogParser.java
+++ src/api/XmlLogParser.java
@@ -5,14 +5,20 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Map;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import bean.Log;
 
@@ -20,10 +26,12 @@ import bean.Log;
 public class XmlLogParser {
 
 	public static ArrayList<Log> parse(String filePath) {
+
 		ArrayList<Log> logs = new ArrayList<Log>();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-		
+
 		try {
+			XmlLogParser.validateXMLSchema("files/template.xsd", filePath);
 			
 			File fXmlFile = new File(filePath);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -32,7 +40,7 @@ public class XmlLogParser {
 			doc.getDocumentElement().normalize();
 
 			NodeList nRequests = doc.getElementsByTagName("request");
-			
+
 			for (int iRequest = 0; iRequest < nRequests.getLength(); iRequest++) {
 				Log log = new Log();
 				Node nRequest = nRequests.item(iRequest);
@@ -50,13 +58,13 @@ public class XmlLogParser {
 							log.addHeader(eHeader.getElementsByTagName("key").item(0).getTextContent(), eHeader.getElementsByTagName("value").item(0).getTextContent());
 						}
 					}
-					
-					NodeList nBody = eRequest.getElementsByTagName("body");
-//					NamedNodeMap nBodyAttrs = nBody.item(0).getAttributes();
-//					log.bodyType = nBodyAttrs.getNamedItem("type").getNodeValue(); 
-//					log.bodyFormat = nBodyAttrs.getNamedItem("format").getNodeValue();
+
+					NodeList nBody = eRequest.getElementsByTagName("content-raw");
+					//					NamedNodeMap nBodyAttrs = nBody.item(0).getAttributes();
+					//					log.bodyType = nBodyAttrs.getNamedItem("type").getNodeValue(); 
+					//					log.bodyFormat = nBodyAttrs.getNamedItem("format").getNodeValue();
 					log.body = nBody.item(0).getTextContent();
-					
+
 					logs.add(log);
 				}
 			}
@@ -64,6 +72,13 @@ public class XmlLogParser {
 			e.printStackTrace();
 		}
 		return logs;
+	}
+
+	public static void validateXMLSchema(String xsdPath, String xmlPath) throws Exception{
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema = factory.newSchema(new File(xsdPath));
+		Validator validator = schema.newValidator();
+		validator.validate(new StreamSource(new File(xmlPath)));	
 	}
 
 }
--- src/replay.java
+++ src/replay.java
@@ -9,19 +9,12 @@ import bean.Log;
 public class replay {
 
 	public static void main(String[] args) throws IOException {
-		String basePath = new File("").getAbsolutePath();
-		String filename = basePath + "/files/log-google-10.xml";
+		String filename = "files/log-google-10.xml";
 		if(args.length == 1) {
-			//Case absolute path (C:/ ou D:/)
-			if(filename.substring(1,1) != ":") {
-				filename = basePath + args[0];
-			} else {
-				filename = args[0];
-			}
+			filename = args[0];
 		}
 		HttpReplay tester = new HttpReplay();
 
-		//ArrayList<Log> logs = JsonLogParser.parse("/files/requests.json");
 		ArrayList<Log> logs =  XmlLogParser.parse(filename);
 		tester.start(logs);
 	}
